494cd9bfdc1e91f1bdcc0897c958be3e299d24fc
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2022-08-28 20:37:13 +0300

    Added current weather and date

--- src/app.js
+++ src/app.js
@@ -1,1 +1,81 @@
+let now = new Date();
 
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let day = days[now.getDay()];
+
+let currentDay = document.querySelector("#day");
+currentDay.innerHTML = day;
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let date = now.getDate();
+if (date === 1 || date === 21 || date === 31) {
+  date = `${date}st`;
+} else {
+  if (date === 2 || date === 22) {
+    date = `${date}nd`;
+  } else {
+    if (date === 3 || date === 23) {
+      date = `${date}rd`;
+    } else {
+      date = `${date}th`;
+    }
+  }
+}
+let currentDate = document.querySelector("#date");
+currentDate.innerHTML = `${month}, ${date}`;
+
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let currentHour = document.querySelector("#hour");
+currentHour.innerHTML = `${hour}:${minutes}`;
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "3cfbe117a0a2b535314cc626699c0963";
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
