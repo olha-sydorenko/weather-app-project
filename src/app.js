cf7f2322d6fc8d5cc4ab0f391aac8a371e81b3d2
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2022-08-28 21:08:59 +0300

    Weather icon added

--- src/app.js
+++ src/app.js
@@ -66,12 +66,15 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "3cfbe117a0a2b535314cc626699c0963";
