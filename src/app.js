963b85b2c264af6018922121a10f10c7c16f961f
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2022-09-11 11:37:40 +0300

    Added forecast API call

--- src/app.js
+++ src/app.js
@@ -60,7 +60,7 @@ if (minutes < 10) {
 let currentHour = document.querySelector("#hour");
 currentHour.innerHTML = `${hour}:${minutes}`;
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Mon", "Tue", "Wed", "Thu"];
@@ -89,6 +89,12 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+function getForecast(coordinates) {
+  let apiKey = "3cfbe117a0a2b535314cc626699c0963";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -110,6 +116,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -149,4 +157,3 @@ fahrLink.addEventListener("click", convertFahr);
 let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", convertCels);
 search("Kyiv");
-displayForecast();
