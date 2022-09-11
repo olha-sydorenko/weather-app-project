70daeb5f31bf63b20db199e0953331c95310e1ac
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2022-09-11 14:23:07 +0300

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -60,30 +60,40 @@ if (minutes < 10) {
 let currentHour = document.querySelector("#hour");
 currentHour.innerHTML = `${hour}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
-
-  let days = ["Mon", "Tue", "Wed", "Thu"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
   <div class="col-2">
-    <div class="weather-forecast-date">${day}</div>
-    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+    <img src="http://openweathermap.org/img/wn/${
+      forecastDay.weather[0].icon
+    }@2x.png"
      alt="" width="42"/>
   <div class="weather-forecast-temp">
    <span class="weather-forecast-temp-max"> 
-     18°
+     ${Math.round(forecastDay.temp.max)}°
    </span> 
    <span class="weather-forecast-temp-min">
-     12°
+     ${Math.round(forecastDay.temp.min)}°
     </span> 
   </div>
   </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
